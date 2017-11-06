159c2a79664167f08f67cb37323099dc32f9bbb8
Isma Perez <iperez@qmetric.co.uk>
2017-11-06 16:18:01 +0000

    initial version

---
+++ index.js
@@ -0,0 +1,55 @@
+var fs = require('fs');
+var uuid = require('uuidv4');
+/**
+ * Module dependencies.
+ */
+
+var Base, log
+
+if (typeof window === 'undefined') {
+  // running in Node
+  Base = require('mocha').reporters.Base
+  log = console.log
+} else if(window.Mocha && window.Mocha.reporters && window.Mocha.reporters.Base) {
+  // running in browser (possibly phantomjs) but without require
+  Base = window.Mocha.reporters.Base
+  log = console.log
+} else {
+  // running in mocha-phantomjs
+  Base = require('./base')
+  log = function(msg) { process.stdout.write(msg + '\n') }
+}
+
+/**
+ * Expose `Teamcity`.
+ */
+
+if (typeof window !== 'undefined' && window.Mocha && window.Mocha.reporters) {
+  window.Mocha.reporters.NightwatchIstanbul = NightwatchIstanbul
+}else{
+  exports = module.exports = NightwatchIstanbul
+}
+
+/**
+ * Initialize a new `Nightwatch+Mocha istanbul` reporter.
+ *
+ * @param {Runner} runner
+ * @api public
+ */
+
+function NightwatchIstanbul(runner) {
+  Base.call(this, runner)
+
+  runner.on('test end', function(test) {
+    process.stdout.write('.');
+
+    var client = this.test.ctx._runnable._nightwatch['@client'];
+
+    client.api.execute(function() {
+      return window.__coverage__;
+    }, [], function(response) {
+      fs.writeFileSync('.nyc_output/' + uuid() + '.json', JSON.stringify(response.value));
+    });
+  })
+}
+
